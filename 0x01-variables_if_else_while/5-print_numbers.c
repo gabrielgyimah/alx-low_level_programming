5238075ae6045f3c9c9aca1d69e7c73ca5b6fc2f
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-02-17 10:11:11 -0800

    program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Main begins here
+ *
+ * Description: prints of alphabets in lowercase with putschar function
+ *
+ * Return: Returns 0 when the mai function is successful
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}

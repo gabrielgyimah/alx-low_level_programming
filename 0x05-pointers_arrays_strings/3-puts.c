d01f7f93b42eaa73528e909e7579aea233081c66
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-03-06 13:51:25 -0800

    A  file that contains the password for the crackme2 executable.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,14 +8,14 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
+        int i = 0;
 
-	while (str[i])
-	{
-		_putchar((str[i]));
+        while (str[i])
+        {
+                _putchar((str[i]));
 
-		i++;
-	}
+                i++;
+        }
 
-	_putchar('\n');
-}
+        _putchar('\n');
+} 

4f901ce3dd5eba7643c487dd62e775c9bb7d7304
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-03-08 04:31:45 -0800

    A recursive function that prints a string in reverse.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,14 +11,11 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		return;
-	}
-
-	else
-	{
-		_print_rev_recursioni(s);
+		_print_rev_recursioni(s + 1);
 		_putchar(*s);
 	}
+	else
+		return;
 }

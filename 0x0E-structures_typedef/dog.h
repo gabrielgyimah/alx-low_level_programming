2c7cdad364042de26885a48ec105de544b3e274c
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-03-20 07:11:07 -0700

    Fixing bugs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef TASK
-#define TASK
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Defines a struct template
@@ -10,9 +10,14 @@
 
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+        char *name;
+        float age;
+        char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif

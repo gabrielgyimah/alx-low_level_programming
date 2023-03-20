0a9695ca3b5e63d5685ae0d5a62b8338f0b2b91b
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-03-20 05:28:10 -0700

    A new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This readme ffile contains documentation on the 0x0E-structures_typedef project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef TASK
+#define TASK
+
+/* struct dog - Defines a struct template */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

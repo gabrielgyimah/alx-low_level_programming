d2a730030a0eebcca32dbdedb0ad6ae49587fdf1
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-04-10 03:31:28 -0700

    A  function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+unsigned int power(unsigned int base, unsigned int exponent);
+
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  *
@@ -10,19 +12,20 @@
 
 unsigned int binary_to_uint(char *arr)
 {
-	unsigned int i, count = 0, y, convert = 0, actual_value = 0, exponent = 0;
+	unsigned int i, count = 0, y, convert = 0, actual_value = 0, exponent = 1;
 	unsigned int base = 2;
+
+	if (arr == NULL)
+	{
+		return (0);
+	}
 
 	for (i = 0; arr[i] != '\0'; i++)
 	{
 		count++;
 	}
 
-	if (arr == NULL)
-	{
-		return (0);
-	}
-	for (y = count; y > 0; y--)
+	for (y = count - 1; y >= 0; y--)
 	{
 		convert = arr[y - 1] - '0';
 		if (convert != 1 && convert != 0)
@@ -31,35 +34,9 @@ unsigned int binary_to_uint(char *arr)
 		}
 		else
 		{
-			actual_value = actual_value + (convert * power(base, exponent));
-			exponent++;
+			actual_value = actual_value + (convert * exponent);
+			exponent = exponent + exponent;
 		}
 	}
 	return (actual_value);
-}
-
-/**
- * power - calculates the power of base to the exponent
- *
- * @base: the base value
- * @exponent: the exponential value
- *
- * Return: the power of the base to the exponent
- */
-
-unsigned int power(unsigned int base, unsigned int exponent)
-{
-	unsigned int result = 1;
-
-	while (exponent > 0)
-	{
-		if (exponent % 2 == 1)
-		{
-			result *= base;
-		}
-
-		base *= base;
-		exponent /= 2;
-	}
-	return (result);
 }

e1958959c8b25b51047db8e6d486fd2a58594313
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-06-09 11:44:22 -0700

    Hash Tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -13,9 +13,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int key_from_djb2;
 
-	if (size <= 0 || key == NULL || key[0] == '\0')
-		exit(EXIT_FAILURE);
-
 	key_from_djb2 = hash_djb2(key);
 
 	return (key_from_djb2 % size);

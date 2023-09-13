7b1a08f2462c71443df9007c04d0c39593d0f8eb
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-09-13 01:55:49 -0700

    Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,57 +1,56 @@
-#include <stdio.h>
 #include "search_algos.h"
 
 /**
- * binary_search - searches an array using binary search algorithm.
- * @array: array to be searched
- * @size: size of array to be searched
- * @value: value to be searched for
- *
- * Return: index of value (Success), -1 failure.
+ * binary_search - Searches for a value in a sorted array
+ * of integers using the Binary search algorithm
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in array
+ * @value: Item we are searching for 
+ * Return: Index of array where value is found
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t start, end;
-	size_t middle;
-
-	start = 0;
-	end = size - 1;
+	size_t mid, start = 0, end = size - 1;
 
 	if (!array || !value)
 		return (-1);
-
+	
 	while (start <= end)
 	{
-		printf("Searching in array: ");
+		printf("Searching in array : ");
 		print_array(array, start, end);
-		middle = (end + start) / 2;
-		if (array[middle] == value)
-			return (middle);
-		else if (array[middle] < value)
-			start = middle + 1;
+		mid = (start + end) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			start = mid + 1;
 		else
-			end = middle - 1;
+			end = mid - 1;
 	}
 	return (-1);
 }
 
 /**
- * print_array - prints a given array
- * @array: Array to be printed
- * @start: printing starting point
- * @stop: stopping point for printing
- *
- * Return: nothing
+ * print_array - Prints out the elements of an array
+ * @start: The index at which we start printing
+ * @end: The index at which we stop printing
+ * Return: Void
  */
-void print_array(int *array, int start, int stop)
-{
-	int i;
 
+void print_array(int *array, int start, int end)
+{
+	int i = start;
+	
 	if (array)
 	{
-		for (i = start; i < stop; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
+		while (i < end)
+		{
+			printf("%d", array[i]);
+			if (i < end - 1)
+				printf(",");
+			i++;
+		}
+		printf("\n");
 	}
 }
